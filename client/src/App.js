650d40356adf96328c33ba2b2e048da071130e89
penelopeundercover <mjseiwert@outlook.com>
2022-11-28 21:19:41 -0600

    app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import "./public/loginpage";
+import "./public/signuppage";
+import "./public/homepage";
 
 function App() {
   return (
     <div className="App">
       <loginpage />
       <signuppage />
+      <homepage />
     </div>
   );
 }
